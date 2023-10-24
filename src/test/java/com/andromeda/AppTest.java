94ce1ab47aea707330c4df8e619190712c8469c2
Andromeda <andromeda951@gmail.com>
2023-10-25 00:31:02 +0700

    Create automation login

--- src/test/java/com/andromeda/AppTest.java
+++ src/test/java/com/andromeda/AppTest.java
@@ -1,6 +1,7 @@
 package com.andromeda;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -19,10 +20,12 @@ public class AppTest
         driver.manage().window().maximize();
         driver.get(baseUrl);
         
+        driver.findElement(By.id("email")).sendKeys("tdd-selenium@gmail.com");
+        driver.findElement(By.id("password")).sendKeys("tdd-selenium");
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
 
 
 
         // driver.quit();
-
     }
 }
